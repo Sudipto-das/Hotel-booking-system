e4b99ad9c9b508e5ce2f91b9b7393d580f35705b
sudipto das <sudiptodas877@gmail.com>
2026-03-22 09:21:26 +0530

    fix chngs

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser")
 app.use(cookieParser())
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://hotel-booking-system-6svkxeges-sudiptodas-projects.vercel.app",
         credentials: true
     }
 ));
