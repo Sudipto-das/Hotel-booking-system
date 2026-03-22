9b672c8dc725294ea73547b593691072f50bb263
sudipto das <sudiptodas877@gmail.com>
2026-03-22 17:35:08 +0530

    fix

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -71,8 +71,8 @@ const userLoginController = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true, // Set to true in production with HTTPS
-      sameSite: "none" ,// "None" requires secure flag
+      secure: false, // Set to true in production with HTTPS
+      sameSite: "lax" ,// "None" requires secure flag
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
 
